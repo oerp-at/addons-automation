efa0c5172d8d24f763b97f1948d4748e20256817
Martin Reisenhofer <martin.reisenhofer@funkring.net>
2020-06-12 10:27:51 +0200

    [IMPL] added automation module

---
+++ automation/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Automation',
+    'version': '13.0.1.0.0',
+    'summary': 'Provide business and devop automation framework',
+    'category': 'Generic Modules',
+    'author': 'funkring',
+    'maintainers': ['funkring'],
+    'website': 'https://github.com/oerp-at',
+    'license': 'LGPL-3',
+    'contributors': [
+        'funkring',
+    ],
+    'depends': [
+        '',
+    ],
+    'external_dependencies': {
+        'python': [
+            '',
+        ],
+    },
+    'data': [
+        '',
+    ],
+    'installable': True
+}
